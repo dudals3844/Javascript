e40a7c156144b46e88f940e2d5e49e18a01d319b
Young Min <dudls3844@gmail.com>
2019-11-09 10:18:27 +0000

    learn variable type

---
+++ study.js
@@ -0,0 +1,18 @@
+var intNum = 10;
+var floatNnm = 0.1;
+
+var singleQuoteStr = 'single';
+var doubleQuoteStr = "hello";
+var boolVar = true;
+var nullVar = null;
+var emptyVar;
+
+console.log(
+    typeof intNum,
+    typeof nullVar,
+    typeof singleQuoteStr,
+    typeof doubleQuoteStr,
+    nullVar,
+    emptyVar
+
+    );
