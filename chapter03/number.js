4ab03b262374c9beebec2d431236d8f657553193
Young Min <dudls3844@gmail.com>
2019-11-09 10:26:19 +0000

    learn string and number

---
+++ chapter03/number.js
@@ -0,0 +1,6 @@
+var num = 5/2;
+
+//javascript는 숫자 타입을 구별 안함
+console.log(num);
+//소수 부분을 버림
+console.log(Math.floor(num));
---
+++ chapter03/string.js
@@ -0,0 +1,6 @@
+var str = 'test';
+console.log(str[0], str[1], str[2], str[3]);
+
+//문자열 첫글자를 대문자로 변경
+str[0] = 'T';
+console.log(str)
